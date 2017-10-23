f99108b793ff6778df63f576af5a0445b97c34f6
chertenokru <admin@chertenok.ru>
2017-10-23 22:00:04 +0300

    -

--- RepositoryClient/src/ru/chertenok/filerepository/client/Main.java
+++ RepositoryClient/src/ru/chertenok/filerepository/client/Main.java
@@ -5,9 +5,9 @@ import ru.chertenok.filerepository.client.util.Util;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Util.getHashCode("rrgrgrgrgr",Util.HashCode.MD5));
-        System.out.println(Util.getHashCode("rrgrgrgrgr",Util.HashCode.SH1));
-        System.out.println(Util.getHashCode("rrgrgrgrgr",Util.HashCode.SH256));
-        System.out.println(Util.getHashCode("rrgrgrgrgr",Util.HashCode.SHA512));
-        
+        System.out.println(Util.getHashCode("rrgrgrgrgr", Util.HashCode.MD5));
+        System.out.println(Util.getHashCode("rrgrgrgrgr", Util.HashCode.SH1));
+        System.out.println(Util.getHashCode("rrgrgrgrgr", Util.HashCode.SH256));
+        System.out.println(Util.getHashCode("rrgrgrgrgr", Util.HashCode.SHA512));
+    }
 }
